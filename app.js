63906617b17fac7ba3278fe3a8518c8e62224ee6
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-30 12:59:49 -0800

    playing with $log service

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope) {
-    
+myApp.controller('mainController', function($scope, $log) {
+  $log.log('Hello World');
+  $log.info('This is info');
+  $log.warn('Warning');
+  $log.debug('Some issues');
+  $log.error('That was an error.')
 });
