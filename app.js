93c992d799c3ee7594de4753ee93efca246bd587
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-30 16:06:36 -0800

    added comments

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
 myApp.controller('mainController', function($scope, $log, $filter) {
   $scope.name = 'John';
+  //Filters Making $scope.name uppercase
   $scope.formattedname = $filter('uppercase')($scope.name);
   $log.info($scope.name);
   $log.info($scope.formattedname);
